ca9ba607dea414b5c9d7a503bb67eb62eb58061a
Karen Benites Segura <klbenites96@gmail.com>
2025-02-06 12:27:15 -0500

    modified horizontal carousel cards from recommendations'section, including html and css files. Also added transitions to all links and buttons of desktop version

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,6 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'SanFrancisco': "url('https://raw.githubusercontent.com/Karen-Benites/PLATZI-TRAVEL/refs/heads/main/public/imgs/sanFrancisco.jpg')",
-        'SanFranciscoDesktop': "url('https://raw.githubusercontent.com/Karen-Benites/PLATZI-TRAVEL/refs/heads/main/public/imgs/sanFranciscoDesktop.jpg')",
         'SearchIcon': "url(https://raw.githubusercontent.com/Karen-Benites/PLATZI-TRAVEL/87dcccf6971db429976a3ceea6512bda8b2e57a9/public/imgs/icons/SearchIcon.svg)",
         'moon': 'url(https://raw.githubusercontent.com/Karen-Benites/PLATZI-TRAVEL/87dcccf6971db429976a3ceea6512bda8b2e57a9/public/imgs/icons/moon.svg)',
       },
@@ -23,7 +21,7 @@ module.exports = {
       boxShadow: {
         'custom-normal': "0px 2px 7px 0px rgba(0, 0, 0, 0.25)",
         'custom-shadow-focus': "inset 0px 0px 1px 1px rgba(204, 45, 74, 0.9)",
-        'card_hover_shadow': "0px 0px 9px 2px rgba(102, 102, 102, 0.5)"
+        'card_hover_shadow': "0px 0px 9px 2px rgba(102, 102, 102, 0.55)"
       },
 
       backgroundSize: {
